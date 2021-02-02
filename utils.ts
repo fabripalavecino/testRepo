7c9f76778a1926656d6a8f6aa852238ba65af9ac
Fabricio Leonel Palavecino <fabricio.palavecino@globant.com>
2021-02-02 16:36:20 -0300

    feat(lambdas):delete all from  utils file PLM-12

--- utils.ts
+++ utils.ts
@@ -1,10 +1,1 @@
-function isValidPayload(payload: any) {
-  const isObjReg = /^(\{(\s*.)*\}|\[(\s*.)*\])$/m;
-  return isObjReg.test(payload);
-}
 
-function addDoubleQuotes(str: string) {
-  return str.replace(/(\{|,)\s?(\"?([a-zA-Z-_]+)\"?)(?=\s?:)/, '$1"$3"');
-}
-
-export { isValidPayload, addDoubleQuotes };
