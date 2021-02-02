15912ec29d3153ff3244e4afe29b7cfff91eb7f5
Fabricio Leonel Palavecino <fabricio.palavecino@globant.com>
2021-02-02 13:57:40 -0300

    feat(lambdas):add function from utils file PLM-11

--- utils.ts
+++ utils.ts
@@ -1,5 +1,10 @@
+function isValidPayload(payload: any) {
+  const isObjReg = /^(\{(\s*.)*\}|\[(\s*.)*\])$/m;
+  return isObjReg.test(payload);
+}
+
 function addDoubleQuotes(str: string) {
   return str.replace(/(\{|,)\s?(\"?([a-zA-Z-_]+)\"?)(?=\s?:)/, '$1"$3"');
 }
 
-export { addDoubleQuotes };
+export { isValidPayload, addDoubleQuotes };
