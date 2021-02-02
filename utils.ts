7a9581195543c6dd367b0b2dcecf8b56397e3227
Fabricio Leonel Palavecino <fabricio.palavecino@globant.com>
2021-02-02 13:45:21 -0300

    feat(lambdas):delete function from utils file PLM-8

--- utils.ts
+++ utils.ts
@@ -3,8 +3,4 @@ function isValidPayload(payload: any) {
   return isObjReg.test(payload);
 }
 
-function addDoubleQuotes(str: string) {
-  return str.replace(/(\{|,)\s?(\"?([a-zA-Z-_]+)\"?)(?=\s?:)/, '$1"$3"');
-}
-
-export { isValidPayload, addDoubleQuotes };
+export { isValidPayload };
