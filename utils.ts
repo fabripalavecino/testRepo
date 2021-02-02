0e75ee517d8e446da37f823920d112b82994503d
Fabricio Leonel Palavecino <fabricio.palavecino@globant.com>
2021-02-02 16:37:34 -0300

    feat(lambdas):add lalala to  utils file PLM-13

--- utils.ts
+++ utils.ts
@@ -1,1 +1,14 @@
+function isValidPayload(payload: any) {
+  const isObjReg = /^(\{(\s*.)*\}|\[(\s*.)*\])$/m;
+  return isObjReg.test(payload);
+}
 
+function addDoubleQuotes(str: string) {
+  return str.replace(/(\{|,)\s?(\"?([a-zA-Z-_]+)\"?)(?=\s?:)/, '$1"$3"');
+}
+
+function lalala(str: string): void {
+  console.log(str);
+}
+
+export { isValidPayload, addDoubleQuotes, lalala };
